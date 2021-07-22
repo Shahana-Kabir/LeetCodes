1a498437b1ae51c233ceca7e7d3a94f731616a80
Shahana Kabir <shahana.kabir@gmail.com>
2021-07-22 11:52:43 -0700

    checking if a string is palindrome or not

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,18 @@
-var x = 123;
+var isPalindrome = function(x){
+    let newArray = Array.from(x); 
+    var len = newArray.length;
+    var revArray = newArray.reverse();
+    for(i = 0;i<(len-1);i++){
+        if(newArray[i] === revArray[len-1]){
+            return true;
+        }
+        else {
+            return false
+        };
+    }
+
+  
+}
+
+var result = isPalindrome('shanu');
+console.log(result);
