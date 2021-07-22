3f57996f46d3d25de3bc028aff5489e14fdf54b6
Shahana Kabir <shahana.kabir@gmail.com>
2021-07-22 16:16:01 -0700

    integer palindrome checking

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,46 @@
-var isPalindrome = function(x){
-    let newString = x.toLowerCase();
-    let newArray = Array.from(newString); 
+// var isPalindrome = function(x){
+//     let newString = x.toLowerCase();
+//     let newArray = Array.from(newString); 
     
-    var len = newArray.length;
+//     var len = newArray.length;
+//     var revArray = newArray.reverse();
+//     for(i = 0;i<(len-1);i++){
+//         if(newArray[i] === revArray[len-1]){
+//             return true;
+//         }
+//         else {
+//             return false
+//         };
+//     }
+  
+// }
+
+// var result = isPalindrome('Shanu');
+// console.log(result);
+
+////// integer plaindrome checking
+
+var isPalindrome = function(x){
+    let generatedArray = [];
+    let sNumber = x.toString();
+    var newArray = Array.from(sNumber);
+    let len = newArray.length;
     var revArray = newArray.reverse();
-    for(i = 0;i<(len-1);i++){
+    
+    
+    for (let i = 0;i<(len-1);i++ ){
         if(newArray[i] === revArray[len-1]){
-            return true;
-        }
-        else {
-            return false
-        };
+                        return 'its a palindrome';
+                    }
+                    else {
+                        return 'not a palindrome'
+                    };
+        
+
     }
 
+    
   
 }
-
-var result = isPalindrome('Shanu');
+var result = isPalindrome(1221);
 console.log(result);
