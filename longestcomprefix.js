3170148655dff8175ae24bd5a7c3c4b483ea79b4
Shahana Kabir <shahana.kabir@gmail.com>
2021-07-24 16:26:25 -0700

    longest prefix from a set of strings

--- longestcomprefix.js
+++ longestcomprefix.js
@@ -1,9 +1,24 @@
-var commonPrefix = function(x){
-var string1 = Array.from(x[0]);
-return (string1.length);
+var commonPrefix = function (strings) {
+    var prefix = "";
+    const firstString = strings[0];
+
+    for (let i = 0; i < firstString.length; i++) {
+        for (let wordIndex = 1; wordIndex < strings.length; wordIndex++) {
+            if (strings[wordIndex].charAt(i) !== firstString.charAt(i)) {
+
+
+                return prefix;
+            }
+        }
+
+        prefix += firstString.charAt(i);
+    }
+    return prefix;
+
 
 }
 
-var result = commonPrefix(['flower', 'flow', 'flight']);
+
+var result = commonPrefix(['flower', 'flow', 'floght']);
 
 console.log(result);
